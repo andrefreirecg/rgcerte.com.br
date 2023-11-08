bb76e310906eea8d9279377d5d629cedb20cf03c
Andre Freire <andrefreirecg@gmail.com>
2023-11-07 21:57:49 -0300

    care_implement_function

--- wp-content/themes/rgcerte/functions.php
+++ wp-content/themes/rgcerte/functions.php
@@ -5,3 +5,13 @@ use Makersolucoes\Theme\Core;
 require_once __DIR__ . "/vendor/autoload.php";
 
 $core = new Core();
+function revslider_shortcode($atts) {
+    if (isset($atts['id'])) {
+        $slider_id = $atts['id'];
+        add_revslider($slider_id);
+    } else {
+        return 'Por favor, especifique o ID do slider no shortcode.';
+    }
+}
+
+add_shortcode('meu_slider', 'revslider_shortcode');
